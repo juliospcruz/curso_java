f1e1aa9435f57584c17050bca4b137715400d043
Júlio César Cruz <jc.spc2017@gmail.com>
2026-01-15 23:56:44 -0300

    Object_toString

--- controller_of_storage/src/application/Program.java
+++ controller_of_storage/src/application/Program.java
@@ -21,7 +21,10 @@ public class Program {
 		System.out.println("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity + ", " + product.totalValueInStock());
+		
+		System.out.println(product);
+		
+		
 		
 		
 		sc.close();
