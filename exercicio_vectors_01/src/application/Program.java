0bbf23bda482d53b7b11aad62837df53f10fbc19
Júlio César Cruz <jc.spc2017@gmail.com>
2026-01-25 15:55:10 -0300

    exercicio_vectors_01

---
+++ exercicio_vectors_01/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int N = sc.nextInt();
+
+        int[] vetor = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("Digite um numero: ");
+            vetor[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        System.out.println("NUMEROS NEGATIVOS:");
+
+        for (int i = 0; i < N; i++) {
+            if (vetor[i] < 0) {
+                System.out.println(vetor[i]);
+            }
+        }
+
+        sc.close();
+    }
+
+}
